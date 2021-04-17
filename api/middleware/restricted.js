1f11f2fde92d12d16cf57516b249446ef21bde7f
ngwatso <ngwatson79@yahoo.com>
2021-04-16 22:15:04 -0400

    created restricted middleware

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,14 +1,50 @@
+const jwt = require('jsonwebtoken');
+const secret = require('../secrets');
+
 module.exports = (req, res, next) => {
-  next();
-  /*
-    IMPLEMENT
+	/*
+    * IMPLEMENT
 
-    1- On valid token in the Authorization header, call next.
+    * 1- On valid token in the Authorization header, call next.
 
-    2- On missing token in the Authorization header,
-      the response body should include a string exactly as follows: "token required".
+    * 2- On missing token in the Authorization header,
+    *   the response body should include a string exactly as follows: "token required".
 
-    3- On invalid or expired token in the Authorization header,
-      the response body should include a string exactly as follows: "token invalid".
+    * 3- On invalid or expired token in the Authorization header,
+    *   the response body should include a string exactly as follows: "token invalid".
   */
+	try {
+		const token = req.headers.authorization?.split(' ')[1];
+
+		if (token) {
+			jwt.verify(
+				token,
+				secret.jwtSecret,
+				(err, decodedToken) => {
+					if (err) {
+						res.status(
+							401
+						).json({
+							message:
+								'token invalid',
+						});
+					} else {
+						req.decodedToken = decodedToken;
+						next();
+					}
+				}
+			);
+		} else {
+			next({
+				statusCode: 401,
+				message: 'token required',
+			});
+		}
+	} catch (err) {
+		next({
+			statusCode: 500,
+			message: 'error validating credentials',
+			...err,
+		});
+	}
 };
